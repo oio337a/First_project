546ab161aeacb6096a384c2f44aa14312f3eb25c
oio337a <oio337a@gmail.com>
2021-08-16 15:40:02 +0900

    modified

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-const player = {
-    name : "yongmin",
-    point: 10,
-    fat: true,
-};
-console.log(player);
-console.log(player.name);
-player.name
+const title = document.querySelector(".hello h1");
 
+function titleClick() {
+    title.style.color = "blue";
+}
+
+function mouseEnter() {
+    title.innerText = "Mouse is here!";
+}
+
+function mouseLeave() {
+    title.innerText = "Mouse is gone!";
+}
+
+title.addEventListener("click", titleClick);
+title.addEventListener("mouseenter", mouseEnter);
+title.addEventListener("mouseleave", mouseLeave);
