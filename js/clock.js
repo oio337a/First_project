1cee0d68f2ee324feb87fa0eae49a5cbf9274da8
oio337a <oio337a@gmail.com>
2021-08-17 16:58:55 +0900

    inserted random quote

--- js/clock.js
+++ js/clock.js
@@ -9,6 +9,6 @@ function getClock() {
     clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
-getClock()
-setInterval(getClock, 1000);
+getClock() // 첫 화면 로딩 후 1번 시간 받아오기
+setInterval(getClock, 1000); // 1초마다 1번씩 실행.
 
