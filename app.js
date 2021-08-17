c3fbaa73a4545cb2f31ffbab2b586152f81988fb
oio337a <oio337a@gmail.com>
2021-08-17 13:39:02 +0900

    Getting Username

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
-const title = document.querySelector(".hello h1");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
 
-function titleClick() {
-    title.style.color = "blue";
+function loginSubmit(event) {
+    event.preventDefault();
+    const username = loginInput.value;
+    loginForm.classList.add("hidden");
+    console.log(username);
 }
 
-function mouseEnter() {
-    title.innerText = "Mouse is here!";
-}
-
-function mouseLeave() {
-    title.innerText = "Mouse is gone!";
-}
-
-title.addEventListener("click", titleClick);
-title.addEventListener("mouseenter", mouseEnter);
-title.addEventListener("mouseleave", mouseLeave);
+loginForm.addEventListener("submit", loginSubmit);
