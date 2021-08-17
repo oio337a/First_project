47a9d9409e816ee4bfc7853bb4a25d4616cedd0f
oio337a <oio337a@gmail.com>
2021-08-17 14:43:44 +0900

    saving username

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ function loginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
+    localStorage.setItem("username", username);
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
