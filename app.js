ac629c8af4f1ebb26c2eac293cf81512abab8d8f
oio337a <oio337a@gmail.com>
2021-08-17 13:51:27 +0900

    Hello to user

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting")
 
+const HIDDEN_CLASSNAME = "hidden";
 function loginSubmit(event) {
     event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    loginForm.classList.add("hidden");
-    console.log(username);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", loginSubmit);
+
